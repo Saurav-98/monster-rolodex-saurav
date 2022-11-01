27d843f0f50061c40e17985f4f5094d0cc8eb531
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-11-01 19:59:41 +0530

    Searchinput, Filtering monster list

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import './App.css';
 
 class App extends Component {
   constructor() {
+    console.log('1 Contructor');
     super();
 
     this.state = {
       monsters: [
         {
           name: 'Saurav',
+          id: '001',
         },
       ],
+      searchField: '',
     };
   }
 
   componentDidMount() {
+    console.log('3 Component DiD Mount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -30,10 +34,36 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    console.log({ startingArray: this.state.monsters });
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    // console.log(filteredMonsters);
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    console.log('2 Render Function');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          type="search"
+          placeholder="search monsters"
+          className="search-input"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
